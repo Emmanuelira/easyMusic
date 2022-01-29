eb830066d9a68936496f45c5824a37054de97af0
Emmanuelira <emmanuel.nobrega.lira@gmail.com>
2022-01-28 21:48:26 -0300

    Layout da página inicial praticamente finalizado, faltando somente o modal 'about-us'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'easyMusic';
+  estilosMusicais: string[] = [
+    "Forró", "Rock", "Samba", "MPB", "Sertanejo", "Outros"
+  ];
 }
